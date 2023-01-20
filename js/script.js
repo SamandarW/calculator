1ca780876eff0b2a3716a549264db1fda596ee6c
Samandar <murodaliyevsamandarmurodaliyes@gmail.com>
2023-01-20 17:18:32 +0500

    deleted comment

--- js/script.js
+++ js/script.js
@@ -36,12 +36,3 @@ switch(symbol){
 
 }
 alert(result)
-// let namePrompt = prompt('ismingiz nima')
-// let surnamePrompt = prompt('familiyangiz nima')
-// let agePrompt = prompt('yoshingiz nechida')
-// let user = {}
-// user.name = namePrompt
-// user.surname = surnamePrompt
-// user.age = agePrompt
-
-// console.log(user)
