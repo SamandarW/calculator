c8113c8bb2d37caaad0b0eb4b11d47db3a875362
Samandar[FE17] <122789184+SamandarW@users.noreply.github.com>
2023-01-27 17:33:51 +0500

    Add files via upload
    added card number finder

---
+++ homework-7/js/script.js
@@ -0,0 +1,15 @@
+let cardNumber = '9860657899945645'
+function checkNumber(){
+if (cardNumber.startsWith('8600') && cardNumber.length == 16){
+    console.log('Good Job :)')
+    console.log(cardNumber.slice(0,4) + '********' + cardNumber.slice(12,16))
+    console.log('UZCARD')
+    console.log('********')
+}if(cardNumber.startsWith('9860') && cardNumber.length == 16){
+    console.log('Excellent :)')
+    console.log(cardNumber.slice(0,4) + '********' + cardNumber.slice(12,16))
+    console.log('HUMO')
+}else{
+    console.log('That was Wrong')
+}}
+checkNumber()
